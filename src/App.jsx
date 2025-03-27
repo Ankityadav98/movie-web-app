732609a621742dc8c9c21beff53cfa593b10e1a7
Ankityadav98 <2016034@iiitdmj.ac.in>
2025-03-28 00:50:14 +0530

    Changes related to App components-state,eventListener etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Card(props) {
+  const [hasLiked, setHasLiked] = React.useState(false);
+  function evtListener() {
+    setHasLiked((hasLiked) => !hasLiked);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="card">
+      <h2>{props.title}</h2>
+      <button onClick={evtListener}>{hasLiked ? "❤️" : "🤍"}</button>
+    </div>
+  );
 }
-
-export default App
+export default function App() {
+  return (
+    <div className="card-container">
+      <Card title="Star Wars" rating={5} iscool={true} />
+      <Card title="Avengers" />
+      <Card title="Hera pheri" />
+    </div>
+  );
+}
